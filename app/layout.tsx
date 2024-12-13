23115bbf1fd8fdeacc9c31afd86299e5523dfdaa
Julianne Murdock <elifcakmak.blog@gmail.com>
2024-12-13 16:04:44 -0500

    last timeeee

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Effortless Quest⚔️",
+  title: "Effortless⚔️Quest",
   description: "Gamify Your Tasks Effortlessly",
 };
 
