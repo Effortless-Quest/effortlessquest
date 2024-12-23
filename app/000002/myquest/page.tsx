cfb7a40e101827879eaf2038243ac5eadc7b0f13
Julianne Murdock <elifcakmak.blog@gmail.com>
2024-12-23 09:13:16 -0500

    github login

--- app/000002/myquest/page.tsx
+++ app/000002/myquest/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import React from 'react';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 // Core components
 import CustomCursor from '../../000000/0-0-cursor/page';
@@ -8,13 +10,22 @@ import QuestNavigation from '../../000000/0-1-QuestHome/navigation';
 import Footer from '../../000000/0-0-footer/footer';
 
 // MyQuest-specific components
-import StickyNotes from '../../000003/StickyNotes/StickyNotes'; // Import StickyNotes component
-import AiCompanion from '../../000003/aicompanion/companion';
+import dynamic from 'next/dynamic';
+
+const StickyNotes = dynamic(() => import('../../000003/StickyNotes/StickyNotes'), { ssr: false });
+const AiCompanion = dynamic(() => import('../../000003/aicompanion/companion'), { ssr: false });
+
 
 const MyQuest = () => {
   const toggleDropdown = () => {
     document.body.classList.toggle("openDropdown");
   };
+
+  const router = useRouter();
+
+  useEffect(() => {
+    // Router-dependent code here
+  }, [router]);
 
   return (
     <div>
