a84538ce1f77f37efb9e24e7c433c8747ea15320
Julianne Murdock <elifcakmak.blog@gmail.com>
2024-12-23 11:43:11 -0500

    firebase

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Initialize Firebase with environment variables
 const firebaseConfig = {
@@ -16,3 +17,4 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Auth
 export const auth = getAuth(app);
+export const db = getFirestore(app); // Export Firestore instance
