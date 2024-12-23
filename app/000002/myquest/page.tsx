082493c41e1db2b1df8aa574e985a2279a77bc75
Julianne Murdock <elifcakmak.blog@gmail.com>
2024-12-23 12:01:50 -0500

    get back the sticky notes

--- app/000002/myquest/page.tsx
+++ app/000002/myquest/page.tsx
@@ -6,6 +6,10 @@ import Footer from "../../000000/0-0-footer/footer";
 import UnderConstruction from "../../000000/0-0-UnderConstruction/UnderConstruction"; // Import the new component
 import ProtectedRoute from "../../000000/1-0-ProtectedRoute/ProtectedRoute"; // Make sure you import your ProtectedRoute component
 
+// MyQuest-specific components
+import StickyNotes from '../../000003/StickyNotes/StickyNotes'; // Import StickyNotes component
+import AiCompanion from '../../000003/aicompanion/companion';
+
 export default function MyQuestPage() {
   const toggleDropdown = () => {
     document.body.classList.toggle("openDropdown"); // Simply toggle the class without needing the state
@@ -13,20 +17,17 @@ export default function MyQuestPage() {
 
   return (
     <div>
-      {/* Use ProtectedRoute to protect the page */}
-      <ProtectedRoute>
-        {/* Import Cursor */}
-        <CustomCursor />
-
-        {/* Navigation */}
-        <QuestNavigation toggleDropdown={toggleDropdown} />
-
-        {/* UnderConstruction */}
-        <UnderConstruction />
-
-        {/* Footer */}
-        <Footer />
-      </ProtectedRoute>
+      <CustomCursor />
+      <QuestNavigation toggleDropdown={toggleDropdown} />
+      <div style={{ display: 'flex' }}>
+        <div style={{ flex: 1 }}>
+          <StickyNotes />
+        </div>
+        <div style={{ flex: 1 }}>
+          <AiCompanion />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
-}
+};
