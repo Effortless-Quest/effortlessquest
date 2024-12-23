5e5bc8bb23899c6621e2dc5b2870ffd4180eeac0
Julianne Murdock <elifcakmak.blog@gmail.com>
2024-12-23 09:55:02 -0500

    ai companion

--- app/000002/myquest/page.tsx
+++ app/000002/myquest/page.tsx
@@ -1,31 +1,33 @@
-"use client"
+"use client";
 
 import React from 'react';
+
+// Core components
 import CustomCursor from '../../000000/0-0-cursor/page';
 import QuestNavigation from '../../000000/0-1-QuestHome/navigation';
 import Footer from '../../000000/0-0-footer/footer';
+
+// MyQuest-specific components
 import StickyNotes from '../../000003/StickyNotes/StickyNotes'; // Import StickyNotes component
+import AiCompanion from '../../000003/aicompanion/companion';
 
 const MyQuest = () => {
   const toggleDropdown = () => {
     document.body.classList.toggle("openDropdown");
   };
 
-  const router = useRouter();
-
-  useEffect(() => {
-    // Router-dependent code here
-  }, [router]);
-
   return (
     <div>
       <CustomCursor />
       <QuestNavigation toggleDropdown={toggleDropdown} />
-        <div>
-          
-        <StickyNotes /> {/* Render the StickyNotes component */}
-          
+      <div style={{ display: 'flex' }}>
+        <div style={{ flex: 1 }}>
+          <StickyNotes />
         </div>
+        <div style={{ flex: 1 }}>
+          <AiCompanion />
+        </div>
+      </div>
       <Footer />
     </div>
   );
